cc818acd9deac2b7315ca456dfdf219c082165b8
João Victor Terzi <jv_ermel@hotmail.com>
2022-05-23 17:00:28 -0300

    Organizando o app com a Column e implementando funções para os botões

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(const PerguntaApp());
+  runApp(PerguntaApp());
 }
 
 class PerguntaApp extends StatelessWidget {
-  const PerguntaApp({Key? key}) : super(key: key);
+  PerguntaApp({Key? key}) : super(key: key);
+
+  final perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é a seu animal favorito?'
+  ];
+
+  void responder() {
+    print('Resposta 1 escolhida!');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Olá Flutter!'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Resposta 2 escolhida!');
+              },
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Resposta 3 escolhida!'),
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
